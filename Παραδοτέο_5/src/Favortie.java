da63f8acd9234f4037402bc71aa0db22d022e59c
eliasalevras <128754241+eliasalevras@users.noreply.github.com>
2023-05-28 13:35:44 +0300

    Delete Favortie.java

--- Παραδοτέο_5/src/Favortie.java
+++
@@ -1,9 +0,0 @@
-public class Favortie extends Products {
-    private String Product_Name;
-    private String Product_Descreption;
-    private double Product_Price;
-    private int Product_ID;
-    private int User_ID;
-    private int Pharmacy_ID;
-
-}
